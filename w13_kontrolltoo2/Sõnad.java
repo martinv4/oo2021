45954bc2a7487a4d52a121c82c8fd890df93e1e0
Klaus Martin Vare <55079748+martinv4@users.noreply.github.com>
2021-05-07 13:25:59 +0300

    teemavalik
    teemavalik

---
+++ w13_kontrolltoo2/Sõnad.java
@@ -0,0 +1,14 @@
+//sõnapikkuste uuring
+//Salvesta uurimiseks vähemalt paarileheküljeline tekst.
+//* Koosta programm, mis loeks kokku, mitu kolmetähelist sõna on tekstis.
+
+//* Koosta graafik, kus ühel teljel on teksti algusest sinnamaani esinenud 
+//kolmetäheliste sõnade arv ning teisel teljel selleks ajaks leitud 
+//kolmetäheliste erinevate sõnade arv.
+
+//* Võta uuringuks tunduvalt pikem tekst. Kuva joonisele sarnased 
+//graafikud 1-10 tähe pikkuste sõnade kohta. Mummukestega tähista
+//joonistele kohad, kus on läbitud veerand, pool ja kolmveerand tekstist.
+public class Sõnad {
+    //teema valitud esialgu :)
+}
