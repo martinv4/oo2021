becb770ec49efe8ddc1e7346ad401e720088b830
Klaus Martin Vare <55079748+martinv4@users.noreply.github.com>
2021-05-07 14:10:45 +0300

    Upload
    Upload

--- w13_kontrolltoo2/Sõnad.java
+++ w13_kontrolltoo2/Sõnad.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 //sõnapikkuste uuring
 //Salvesta uurimiseks vähemalt paarileheküljeline tekst.
 //* Koosta programm, mis loeks kokku, mitu kolmetähelist sõna on tekstis.
@@ -9,6 +11,18 @@
 //* Võta uuringuks tunduvalt pikem tekst. Kuva joonisele sarnased 
 //graafikud 1-10 tähe pikkuste sõnade kohta. Mummukestega tähista
 //joonistele kohad, kus on läbitud veerand, pool ja kolmveerand tekstist.
+
+// Java program to count the 
+// number of charaters in a file
+
+  
 public class Sõnad {
     //teema valitud esialgu :)
+    public static void main(String[] args) throws FileNotFoundException{
+        //read file?
+        File testFile = new File("testtext.txt");
+        FileInputStream testFileInputStream = new FileInputStream(testFile);
+        InputStreamReader testInputStream = new InputStreamReader(testFileInputStream);
+        BufferedReader testReader = new BufferedReader(testInputStream);
+    }
 }
