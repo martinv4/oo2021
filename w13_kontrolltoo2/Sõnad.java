6c8b31147262477b42db8a01d0a5138488325e8f
Klaus Martin Vare <55079748+martinv4@users.noreply.github.com>
2021-05-14 11:28:51 +0300

    Upload
    Upload

--- w13_kontrolltoo2/Sõnad.java
+++ w13_kontrolltoo2/Sõnad.java
@@ -1,4 +1,11 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 //sõnapikkuste uuring
 //Salvesta uurimiseks vähemalt paarileheküljeline tekst.
@@ -20,37 +27,53 @@ public class Sõnad {
     //teema valitud esialgu :)
     public static void main(String[] args) throws IOException{
         //read file?
-        File testFile = new File("testtext.txt");
-        FileInputStream testFileInputStream = new FileInputStream(testFile);
-        InputStreamReader testInputStream = new InputStreamReader(testFileInputStream);
-        BufferedReader testReader = new BufferedReader(testInputStream);
+        //File testFile = new File("testtext.txt");
+        //FileInputStream testFileInputStream = new FileInputStream(testFile);
+        //InputStreamReader testInputStream = new InputStreamReader(testFileInputStream);
+        //BufferedReader testReader = new BufferedReader(testInputStream);
+        Sõnad testing = new Sõnad();
+        testing.SõnadeArv();
+    }
 
-        int sõnadeArv = 0;
-        String text;
         
+    
+    public int sõnadeArv = 0;
+    String text;
+        
+    public void SõnadeArv() throws IOException {
+            File testFile = new File("testtext3.txt");
+            FileInputStream testFileInputStream = new FileInputStream(testFile);
+            InputStreamReader testInputStream = new InputStreamReader(testFileInputStream);
+            BufferedReader testReader = new BufferedReader(testInputStream);
+                
+
         while((text = testReader.readLine()) != null){
-            
-            if(text.length()!=3){
-                
 
-                //System.out.println("3 tähelisi sõnu pole");
-                
-                
-                //System.out.println(text);
-            } else {
-                String[] sõnadeArv2 = text.split("\\s+");
-                sõnadeArv += sõnadeArv2.length;
-                
-                
-                //System.out.println(sõnadeArv2);
+        
+            String[] kõikSõnad = text.split(" ");;
+            for(String sõnad : kõikSõnad){
+
+                if(sõnad.length()==3){
+                    sõnadeArv = sõnadeArv +1;
+                    System.out.println(Arrays.asList(sõnad));
+                    //System.out.println("test/esines sõna");
+                    
+                } else {
+                    
+                }
+                //String kõikSõnad = Arrays.toString();
+                //System.out.println(sõnad.length());
+                //System.out.println(sõnad3);
+
             }
-            
         }
-        if(sõnadeArv==0){
-            System.out.println("3 tähelisi sõnu pole");
-        } else {
-            System.out.println(sõnadeArv);
+        if( sõnadeArv == 0){
+            System.out.println("3tähelisi sõnu ei olnud");
         }
         
+        
+        System.out.println("tekstis oli 3tähelisi sõnu kokku: " + sõnadeArv);
     }
 }
+
+ 
